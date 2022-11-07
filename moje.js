d1271c7c3ee7a95cebf495c0eceee8c6817aae56
eughrat <michal.piernicki@gmail.com>
2022-11-07 06:51:37 +0100

    learning basics

---
+++ moje.js
@@ -0,0 +1,75 @@
+// INTRO
+
+// console.log("dupa")
+// let a = "2";
+// var b = String(3);
+// a = "cos innego";
+// var c = true;
+// var d = null;
+// var e;
+
+// console.log(c);
+
+// MATH
+
+// let a = 6; c = 3;
+// let b = 2;
+// a += 2;
+
+// c = (2 + 2) * 3;
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(c % b);
+
+// INSTRUKCJE WARUNKOWE
+
+// let a = 3;
+// let b = 4;
+// let zwierzeta = ['kot', 'pies', "kon", 2.343];
+// let warzywa = new Array('łosoś', 'tuna');
+
+
+// if (a !== b) {
+//     warzywa[0] = a
+//     console.log(warzywa[0])
+//     console.log(zwierzeta[1])
+// } else if (a === b) {
+//     console.log("co se dzieje")
+// } else if (a == 3) {
+//     console.log('jest nie dobrze bro')
+// } else {
+//     console.log(";aslkjdlaskndoas" + 3)
+// };
+
+// zwierzeta.pop()
+// zwierzeta.shift()
+// zwierzeta.unshift(2.343)
+// zwierzeta.push(2 * "res")
+// console.log(zwierzeta)
+// zwierzeta.splice(1, 3, "twoja stara", "twoj stary", "twojej starej stary stary")
+
+// let dupa = zwierzeta.slice(0, 2)
+
+// console.log(zwierzeta)
+// console.log(zwierzeta.length)
+// console.log(dupa)
+
+
+// OBIEKTY
+
+let osoba = {
+    imie: "Krystian",
+    nazwisko: "BuzałaEloo323",
+    wiek: 33,
+    szkoły: {
+        podstawowa: "SP23",
+        gimnazjum: "KP02",
+        liceum, "break"
+    }
+};
+
+osoba["wiek"] = "twoja stara"
+console.log(osoba.imie + " " + osoba.nazwisko + " " + osoba.wiek);
